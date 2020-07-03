dc4397c14abbe5d4cee406d2b8c36ef399918247
Su Specie <sspecie13@gmail.com>
2020-07-02 23:23:20 -0300

    Layouut Nubank e Facebook

---
+++ app/src/main/java/br/com/suspecie/layoutfacebooknubank/FacebookActivity.kt
@@ -0,0 +1,12 @@
+package br.com.suspecie.layoutfacebooknubank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FacebookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facebook)
+    }
+}
---
+++ app/src/main/java/br/com/suspecie/layoutfacebooknubank/NubankActivity.kt
@@ -0,0 +1,12 @@
+package br.com.suspecie.layoutfacebooknubank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NubankActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nubank)
+    }
+}
